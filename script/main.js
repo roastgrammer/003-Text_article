08607946f396de329957dafc9a6d9b507f5c0a69
roastgrammer <roastgrammer@gmail.com>
2022-10-13 10:51:33 +0330

    update 2022

--- script/main.js
+++ script/main.js
@@ -1,11 +1,5 @@
-let share_btn = document.getElementById("share-icon");
-share_btn.addEventListener('click' , ()=>{
-  let option_share = document.getElementById('main-middle');
-  option_share.style.transition = '.3s';
-  if(option_share.style.opacity === '1'){
-    option_share.style.opacity = '0';
-    }  else {
-        option_share.style.opacity = '1';
-
-  } 
+const share = document.getElementById("share-btn");
+share.addEventListener("click" , function() {
+  var opt = document.getElementById("options");
+  opt.classList.toggle("show");
 })
